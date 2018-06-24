7de3b97c7ed9b30663d2b2e788cbc768e8a0a033
tom jones <thomasclinganjones@gmail.com>
2018-06-24 16:48:50 -0700

    Add project files.

---
+++ IDESG2rp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IDESG2rp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
